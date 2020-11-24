eb76b46dc2d27000baab9b319faff73cc81c2762
Gavin56 <gavinobrien56@gmail.com>
2020-11-24 16:04:00 -0500

    Basic menus for all scenarios created.

--- index.js
+++ index.js
@@ -7,14 +7,60 @@ function getUserInput() {
             {
                 type: "list",
                 message: "What would you like to do?",
-                choices: ["Add", "View", "Update", "Quit" ],
+                choices: ["Add", "View", "Update Employee Roles", "Quit"],
                 name: "processChoice"
             }
-        ]);
+        ]).then(function (data) {
+            // console.log(".then works!");
+
+            switch (data.processChoice) {
+                case "Add":
+                    displayAddMenu();
+                    break;
+                case "View":
+                    displayViewMenu();
+                    break;
+                case "Update Employee Roles":
+                    updateRoles();
+                    break;
+                case "Quit":
+                    break;
+            }
+        });
         return data;
-    } catch(error) {
+    } catch (error) {
         console.log(error);
     }
+};
+
+function displayAddMenu() {
+    let data = inquirer.prompt([
+        {
+            type: "list",
+            message: "Select what you would like to add:",
+            choices: ["Add Departments", "Add Roles", "Add Employees"],
+            name: "addChoice"
+        }
+    ]).then(function () {
+        console.log("You successfully chose an add option.")
+    });
+};
+
+function displayViewMenu() {
+    let data = inquirer.prompt([
+        {
+            type: "list",
+            message: "Select what you would like to view:",
+            choices: ["View Departments", "View Roles", "View Employees"],
+            name: "viewChoice"
+        }
+    ]).then(function () {
+        console.log("You successfully chose a view option.")
+    });
+};
+
+function updateRoles() {
+    console.log("Updating roles...");
 };
 
 getUserInput();
